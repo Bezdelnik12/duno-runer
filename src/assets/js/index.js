6ffd8582a97036f353b3de6145826eb0ba7541ae
Bezdelnik12 <75016725+Bezdelnik12@users.noreply.github.com>
2021-04-01 10:28:46 +0300

    Update index.js
    правка багов

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,13 +1,8 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-// extract from chromium source code by @liuwayong
-
 var is_user = false;
 var game_run = false;
 
 /**
- * T-Rex runner.
+ * Dino runner.
  * @param {string} outerContainerId Outer containing element id.
  * @param {Object} opt_config
  * @constructor
